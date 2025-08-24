4d14cd036390b0fd71922bcce80764e74c47caae
Jordan HOAREAU MARION <multitask315@proton.me>
2025-08-24 17:52:24 +0200

    Introduce dedicated devcontainer for all three tools

---
+++ src/ansible-kubernetes-tofu/.devcontainer/ansible/install_ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+# Update package lists and install pipx
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+# Ensure pipx is in the PATH
+pipx ensurepath
+pipx ensurepath --global # optional to allow pipx actions with --global argument
+
+# Install Ansible via pipx (including dependencies)
+pipx install --include-deps ansible
+
+# Install ansible-lint via pipx
+pipx install ansible-lint
+
+# Inject pexpect into the ansible pipx environment
+pipx inject ansible pexpect
